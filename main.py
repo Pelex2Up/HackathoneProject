697fcd4af168a35dc1793ead44ca5f5c0b37abff
Pelex2Up <neksomg123@gmail.com>
2022-09-02 23:54:17 +0300

    костяк

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ bot = telebot.TeleBot("5621023291:AAHMLhmpOOEpA1XRtRWebijBmsYms4neYSA")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, """Добрый день, {0.first_name}! Вас приветствует служба поддержки пользователей.
+Чем я могу Вам помочь?""".format(message.from_user))
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+def user_request(message):
+	bot.reply_to(message, 'Ваш запрос принят. Ожидайте ответ от службы поддержки!')
 
-bot.infinity_polling()
+bot.polling(none_stop=True, interval=0)
